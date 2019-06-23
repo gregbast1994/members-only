18be291db9ccae22adf15351dcc94549e9fc5432
gregbast1994 <gregbast1994@gmail.com>
2019-06-23 11:31:39 -0400

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/destroy'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
