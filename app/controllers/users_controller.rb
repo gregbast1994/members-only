7fbe1e78dc5dfca2165443b7b84906f16e2c6b7a
gregbast1994 <gregbast1994@gmail.com>
2019-06-25 10:12:52 -0400

    restrict pages unless authorized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :correct_user, only: [:edit, :update]
+
     def new
         @user = User.new
     end
@@ -20,9 +22,26 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            redirect_to @user
+            flash[:success] = "Profile updated"
+        else
+            render 'edit'
+        end
+    end
+
     private
+
+    def correct_user
+        @user = User.find(params[:id])
+        flash[:danger] = "Sorry, you have attempted to access an unathorized page"
+        redirect_to root_path unless current_user?(@user)
+    end
+
     def user_params
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
-        end
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,11 @@ module SessionsHelper
         @current_user ||= User.find(session[:user_id])
     end
 
+    def current_user?(user)
+        user == current_user
+    end
+
+
     def logged_in?
         !session[:user_id].nil?
     end
@@ -17,4 +22,5 @@ module SessionsHelper
         session[:user_id] = nil
         @current_user = nil
     end
+
 end
